7e2918aa32dd7aaa94ff6a7aa80874898c72de2c
Jin Kim <jinwoov@gmail.com>
2021-06-01 18:11:26 -0600

    chagned the picture to more rascalness

--- pages/rochi/index.js
+++ pages/rochi/index.js
@@ -4,20 +4,21 @@ import Footer from "../../components/footer/footer.component";
 
 function Mochi() {
 
+    let changePic = true;
     return (
             <>
             < Header />
                 <div className={styles.maindiv}>
-                    {/* <img 
+                    { changePic ? ( <img 
                         className={styles.img} 
                         src="/assets/rochi.jpg" 
                         alt="mochi's picture"
-                    /> */}
+                    /> ) : ( 
                 <img 
                         className={styles.img} 
                         src="/assets/mochi-seal.png" 
                         alt="mochi's picture"
-                    />
+                    /> )}
                 </div>
                 <Footer />
             </>
